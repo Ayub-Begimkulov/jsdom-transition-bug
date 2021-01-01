9d40f735fe09fbc67a7aa48d5bb20e6f181a9e9a
Ayub-Begimkulov <begimkulov963@gmail.com>
2021-01-01 17:12:41 +0300

    made a bug reproduction

---
+++ index.js
@@ -0,0 +1,22 @@
+const { JSDOM } = require("jsdom");
+
+const jsdom = new JSDOM(`<!DOCTYPE html>
+  <html lang="en">
+    <head>
+      <meta charset="UTF-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <title>Document</title>
+      <style>
+        .test {
+          transition: opacity 500ms ease-in-out;
+        }
+      </style>
+    </head>
+    <body>
+      <div class="test"></div>
+    </body>
+  </html>`);
+const { window } = jsdom;
+const el = window.document.querySelector(".test");
+const styles = window.getComputedStyle(el);
+console.log(styles.transitionDelay, styles.transitionDuration);
